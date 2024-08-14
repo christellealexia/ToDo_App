3beb8875c3fdc9382d25aa43b0bbef4bda89255b
Alexia Christelle <christellealexia53@gmail.com>
2024-08-14 17:00:42 +0200

    Implemented a button of adding a todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react"
 
+const App = () => {
+ const[todo, settodo] = useState('')
+ const [todos, settodos] = useState([])
+
+ function Adding () {
+ settodos([...todos, todo])
+  settodo('')
+ }
+ function deleteTodo(indexToDelete) {
+  settodos(todos.filter((_, index) => index !== indexToDelete))
+}
+
+
+
+ let elements = todos.map((todo,index)=> {
+  return (
+    <div key ={index} className=" flex gap-7">
+      <span>{todo}</span>
+      <button className='bg-red-600 px-4 py-4 text-white pb-4' onClick={deleteTodo}>Delete</button>
+    </div>
+  )
+ })
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex flex-col gap-3">
+        <div  className="flex gap-6">
+       <input
+       
+       name= 'todo'
+       type='text'
+       value ={todo}
+       onChange = {(e)=> settodo(e.target.value)}
+       className='px-8 py-3 border border-black pb-3'>
+       </input>
+       <button className='bg-blue-700 px-4 py-4 text-white pb-4' onClick={Adding}>Add</button>
+       </div>
+
+      
+       <h1 className="flex flex-col gap-6">{elements}</h1>
+       </div>
     </>
   )
 }
 
 export default App
+
+
+
